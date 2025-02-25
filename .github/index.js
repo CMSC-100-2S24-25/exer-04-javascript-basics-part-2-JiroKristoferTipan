b43dd536fe6b0990981263ce0ec080d2ff31547b
JiroTipan <jttipan1@up.edu.ph>
2025-02-26 00:01:30 +0800

    added test.js, fixed wrong reading of uuid and functions having wrong variable initializations

--- .github/index.js
+++ .github/index.js
@@ -1,25 +1,26 @@
 //imports
+import { appendFileSync } from 'node:fs';
 import { v1 as uuidv1 } from 'uuid';
-import isEmail from 'validator/lib/isEmail';
+import isEmail from 'validator/lib/isEmail.js';
 
-function generateUniqueID(fname, lname){
+const generateUniqueID = (fname, lname) => {
     //get 1st letter of 1st name, full last name and random 8 long alphanumeric
-    ID1 = fname[0];
-    ID2 = lname;
-    ID3 = uuidv1()[0-8];
+    var ID1 = fname[0];
+    var ID2 = lname;
+    var ID3 = uuidv1().substring(0,8);
 
     //concatenate values and return
-    newID = ID1 + ID2 + ID3;
+    var newID = ID1 + ID2 + ID3;
     return newID;
 }
 
-function addAccount(fname, lname, email, age){
+const addAccount = (fname, lname, email, age) => {
     //make sure all input present
     if (fname == null||lname == null||email == null||age == null){
         return false;
     }
     //check if email provided
-    if (!(validator.isEmail(email))){
+    if (!(isEmail(email))){
         return false;
     }
     //check if 18+
@@ -27,6 +28,16 @@ function addAccount(fname, lname, email, age){
         return false;
     }
     //assuming all conditions met, create user info and save
-    user = fname+","+lname+","+email+","+age+","+generateUniqueID(fname, lname);
+    var userinfo = fname+","+lname+","+email+","+age+","+generateUniqueID(fname, lname)+"\n";
+    //some error handling
+    try {
+    appendFileSync("users.txt", userinfo);
+    console.log("Added info to file!");
+    } catch (err) {
+    console.log("Error with adding info!");
+    } 
+    return true;
 
 }
+
+export {generateUniqueID, addAccount}
---
+++ .github/test.js
@@ -0,0 +1,6 @@
+//import index.js
+import { generateUniqueID, addAccount } from "./index.js";
+
+//run commands
+console.log(generateUniqueID("Jiro", "Tipan"));
+console.log(addAccount("Lord", "Farquaad", "jttipan1@up.edu.ph", 99));
